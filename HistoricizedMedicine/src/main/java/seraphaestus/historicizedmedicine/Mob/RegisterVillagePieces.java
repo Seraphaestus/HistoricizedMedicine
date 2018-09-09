6f84a705273974b5719ed4ec958729f95be29169
Sunconure11 <eyespot7b@aol.com>
2018-09-09 01:14:06 -0400

    Trying something stupid

--- HistoricizedMedicine/src/main/java/seraphaestus/historicizedmedicine/Mob/RegisterVillagePieces.java
+++ HistoricizedMedicine/src/main/java/seraphaestus/historicizedmedicine/Mob/RegisterVillagePieces.java
@@ -8,8 +8,10 @@ public class RegisterVillagePieces {
 
 	public static void init() {
 		VillagerRegistry villageRegistry = VillagerRegistry.instance();
-		villageRegistry.registerVillageCreationHandler(new VillageStall.VillageManager());
-		MapGenStructureIO.registerStructureComponent(VillageStall.class, HMedicineMod.MODID + ":MedicineStall");
+		villageRegistry.registerVillageCreationHandler(new VillageStall1.VillageManager());
+		villageRegistry.registerVillageCreationHandler(new VillageStall2.VillageManager());
+		MapGenStructureIO.registerStructureComponent(VillageStall1.class, HMedicineMod.MODID + ":MedicineStall1");
+		MapGenStructureIO.registerStructureComponent(VillageStall2.class, HMedicineMod.MODID + ":MedicineStall2");
 	}
 
 }
